9fe22b29a4afe1d33762cb8b939122d74441f987
Abie <rockabiemba@gmail.com>
2022-07-23 20:21:55 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //  selecteur css document.querySelector
+  templateUrl: `./app.component.html`,
+  styles: []
+})
+export class AppComponent {
+  title = 'hello-angular!!!!';
+}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent {
+
+  surname = 'Rock';
+
+
+}
